c42ef39b305680019d074774ad7e0a52ba041994
Alina Zeiher <alina.lisa.zeiherweb.de>
2019-05-22 16:32:09 +0200

    ddd

--- Typescript/script.js
+++ Typescript/script.js
@@ -2,14 +2,25 @@
 console.log("Hallo");
 console.log("Was geht");
 window.onload = function () {
+    document.getElementById("ge").addEventListener("click", ChangeName);
+    document.getElementById("Dunkel").addEventListener("click", ChangeClass);
+    window.alert("Klick Mal!");
+    let name = "Ja"; //Ich weiße der Variable den Typ String und den Wert Ja zu // 
+    let name2 = "Ja2";
+    let number1 = 1; //Typ Number//
+    let number2 = 2;
+    let number3 = 3;
+    number2 = 3; //neuer Wert an deklarierte Varbiable// 
+    console.log(name + name);
+    console.log(number1 + number2);
+    console.log(number1 + name);
 };
-let name = "Ja"; //Typ String// 
-let name = "Ja2";
-let number1 = "1"; //Typ Number//
-let number2 = "2";
-let number3 = "3";
-number2 = 3; //neuer Wert an deklarierte Varbiable// 
-console.log(name + name);
-console.log(number1 + number2);
-console.log(number1 + name);
+function ChangeName() {
+    document.getElementById("ge").innerHTML = "Ulala";
+}
+//Klassenänderung//
+function ChangeClass() {
+    document.getElementById("Dunkel").className = "Lampe2";
+}
+//HTML neu erstellen//
 //# sourceMappingURL=script.js.map
--- Typescript/script.ts
+++ Typescript/script.ts
@@ -4,15 +4,17 @@ console.log("Hallo");
 console.log("Was geht");
 
 window.onload = function () {
-}   
+    document.getElementById("ge").addEventListener("click", ChangeName);
+    document.getElementById("Dunkel").addEventListener("click", ChangeClass);
+    window.alert("Klick Mal!")
     
    
 
-let name: string ="Ja"; //Typ String// 
-let name: string ="Ja2";
-let number1: number="1"; //Typ Number//
-let number2: number="2";
-let number3: number="3";
+let name: string ="Ja"; //Ich weiße der Variable den Typ String und den Wert Ja zu // 
+let name2: string ="Ja2";
+let number1: number=1; //Typ Number//
+let number2: number=2;
+let number3: number=3;
 number2 = 3; //neuer Wert an deklarierte Varbiable// 
 console.log(name+name);
 console.log(number1+number2);
@@ -20,16 +22,18 @@ console.log(number1+name);
 
 }
 
+function ChangeName() {
+    document.getElementById("ge").innerHTML = "Ulala";
 
+}
 
+//Klassenänderung//
+function ChangeClass () {
+    document.getElementById("Dunkel").className = "Lampe2";
 
+}
 
-
-
-
-
-
-
+//HTML neu erstellen//
 
 
 
