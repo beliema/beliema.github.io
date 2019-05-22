c41519d5c6c51a90fcf1ab5769dae95618a7ca70
Alina Zeiher <alina.lisa.zeiherweb.de>
2019-05-22 15:38:35 +0200

    mm,m

--- Typescript/script.js
+++ Typescript/script.js
@@ -1,6 +1,9 @@
 console.log("Hallo");
 console.log("Was geht");
-function add(x, y) {
-    return x + y;
-}
-//# sourceMappingURL=script.js.map
+
+
+function numbers() { 
+    var num1 = 2, 
+        num2 = 3;
+    return num1 + num2;
+  }
--- Typescript/script.ts
+++ Typescript/script.ts
@@ -2,7 +2,15 @@ console.log("Hallo");
 console.log("Was geht");
 
 
+string summand1;
+string summand2;
+string SVGAnimatedEnumeration;
 
-function add(x: number, y: number): number {
-    return x + y;
-}
+string summand1 = 23;
+string summand2 = 55;
+
+string summe = summand1 + summand2;
+
+MediaKeySystemAccess.out.printIn("Die Summe der Zahlen ist");
+
+
