926f3325158200fcf9ea37cc52bd0092dd80b17f
Alina Zeiher <alina.lisa.zeiherweb.de>
2019-07-16 17:42:55 +0200

    sdd

--- Finale Abgabe /js/TS.ts
+++ Finale Abgabe /js/TS.ts
@@ -19,7 +19,7 @@ interface Karte;
 
 let Karte: string[] = ["Ratte", "Ed von Schleck", "Ungeziefer", "Kommulitonin", "Beuteltier", "Eichhörnchen"]; // length = 3, da 3 Einträge. Von 0-2.
 
-let Bildquellen: string[] = ["", "loewe.png", "Monster1.png", "Monster3.png", "pinguin.png", "Monster4.jpg"]
+let Bildquellen: string[] = ["b1.JPG", "b2.JPG", "b3.JPG", "b4.JPG", "b5.JPG", "b6.JPG", "b7.JPG", "b8.JPG", "b9.JPG", "ge1.JPG", "ge2.JPG", "ge3.JPG", "ge4.JPG", "ge5.JPG", "ge6.JPG", "ge7.JPG", "ge8.JPG", "ge9.JPG", "gr1.JPG", "gr2.JPG", "gr3.JPG", "gr4.JPG", "gr5.JPG", "gr6.JPG", "gr7.JPG", "gr8.JPG", "gr9.JPG", "R1.JPG", "R2.JPG", "R3.JPG", "R4.JPG", "R5.JPG", "R6.JPG", "R7.JPG", "R8.JPG", "R9.JPG"]
 
 let PushArray: number[]=[];
 
