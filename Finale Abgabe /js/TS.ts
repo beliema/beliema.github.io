015a63957eb71135211b4cb085de653a6c13a9cc
Alina Zeiher <alina.lisa.zeiherweb.de>
2019-07-24 00:07:28 +0200

    mm

--- Finale Abgabe /js/TS.ts
+++ Finale Abgabe /js/TS.ts
@@ -8,7 +8,15 @@ interface Karte {
    object: string; 
 }
 
+interface Spieler1 {
+    SpielerName: string ="Spieler1";
 
+}
+
+interface Spieler2 {
+    GegnerName: string ="Spieler2";
+
+}
 // ------- Variablen -------- //
 //Spiel - Variable: Grundliegende Eigenschaften
 
@@ -17,22 +25,25 @@ let Bildpfad: string;
 let cardHolder = "Kartenstapel"; //ID für Kartenstapel
 let playerName : string = "Spieler1";
 let playerObjects: string = "Karten";
-let playerGegner: string = "Computer";
+let playerGegner: string = "Spieler2";
 let winGame: boolean = false; 
-
-
-
-
+let MaxSpieler: number = 2; 
+let MaxKarten: number = 36;
 
 
 
 // Arrays 
 
-let Bildquellen: string[] = ["b1.JPG", "b2.JPG", "b3.JPG", "b4.JPG", "b5.JPG", "b6.JPG", "b7.JPG", "b8.JPG", "b9.JPG", "ge1.JPG", "ge2.JPG", "ge3.JPG", "ge4.JPG", "ge5.JPG", "ge6.JPG", "ge7.JPG", "ge8.JPG", "ge9.JPG", "gr1.JPG", "gr2.JPG", "gr3.JPG", "gr4.JPG", "gr5.JPG", "gr6.JPG", "gr7.JPG", "gr8.JPG", "gr9.JPG", "R1.JPG", "R2.JPG", "R3.JPG", "R4.JPG", "R5.JPG", "R6.JPG", "R7.JPG", "R8.JPG", "R9.JPG"]
-let PushArray: number[] = [];
-let cardArray = card[] = [];
+let prefix: string [] = ["Rot-", "Grün-", "Blau-", "Gelb-"];   //4 Prefixe für die vier Farben
+let cardNumber: string [] = ["Eins", "Zwei", "Drei", "Vier","Fünf", "Sechs", "Sieben", "Acht", "Neun"]; //Zahlen von 1-9 
 
-let 
+let Bildquellen: string[] = ["b1.JPG", "b2.JPG", "b3.JPG", "b4.JPG", "b5.JPG", "b6.JPG", "b7.JPG", "b8.JPG", "b9.JPG", "ge1.JPG", "ge2.JPG", "ge3.JPG", "ge4.JPG", "ge5.JPG", "ge6.JPG", "ge7.JPG", "ge8.JPG", "ge9.JPG", "gr1.JPG", "gr2.JPG", "gr3.JPG", "gr4.JPG", "gr5.JPG", "gr6.JPG", "gr7.JPG", "gr8.JPG", "gr9.JPG", "R1.JPG", "R2.JPG", "R3.JPG", "R4.JPG", "R5.JPG", "R6.JPG", "R7.JPG", "R8.JPG", "R9.JPG"]
+let AllCards: string[] = ["1Blau", "2Blau", "3Blau", "4Blau", "5Blau", "6Blau", "7Blau", "8Blau", "9Blau", "1Gelb", "2Gelb", "3Gelb", "4Gelb", "5Gelb", "6Gelb", "7Gelb", "8Gelb", "9Gelb", "1Grün", "2Grün", "3Grün", "4Grün", "5Grün", "6Grün", "7Grün", "8Grün", "9Grün", "1Rot", "2Rot", "3Rot", "4Rot", "5Rot", "6Rot", "7Rot", "8Rot", "9Rot"]
+let PushArray: number[] = [];
+let cardArray = Karte[] = [];
+
+let SpielerHand: string[] = ["Eins", "Zwei", "Drei", "Vier", "Fünf"]; 
+let KartenZiehstapel: string[] = []
 
 
 
@@ -48,15 +59,25 @@ window.onload = function () {
 };
 
 
-//fügt demn Spieler neue Items hinzu
-function updatecard(neuesItem: string) {
-    playerItems += ", " + neuesItem;
-}
+
 
 //Funktion: Neue Karte generieren//
 
+function newgame () {
+    let generateCard: number = 36;
+
+}
+
+function getnewcard () {
+
+}
+
+function mixnew () {
+
+}
+
 function generateCard () {
-    let random: number = getRNGNumber(5)+1; //generiert bis zu 5 Karten
+    let random: number = getRNGNumber(36)+1; //generiert bis zu 5 Karten
 
     for (let i: number = 0; i < random; i++) //eine Schleife mit der Endfunktion i
 
@@ -64,7 +85,7 @@ function generateCard () {
     let newCard: string = generateCard();
     
     let newObject: string = generateObject();
-    let Picadress: string: bildpfad;
+    let Picadress: string = Bildpfad;
 
     let newCard: Karte = {
         CardColor: newCardcolor,
@@ -74,11 +95,13 @@ function generateCard () {
 
 };
 
-CardArray.push(newCard);
+cardArray.push(newCard);
 
-console.log(CardArray[CardArray.length -1].);
+console.log(cardArray[cardArray.length -1].);
 
 } 
 updateHTML();
 }
+
+function newCard () {
 
