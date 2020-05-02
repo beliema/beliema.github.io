4a9e661e936f77d68649e77c8e9b2c2bda882105
Alina Zeiher <alina.lisa.zeiherweb.de>
2020-05-02 16:57:43 +0200

    k

--- Semester2/A2/Memorie.js
+++ Semester2/A2/Memorie.js
@@ -1,11 +1,10 @@
 const cards = document.querySelectorAll('.sequencecard');
 
 function flipCard() {
-    this.classList.toggle('flip');
+  this.classList.toggle('flip');
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
-
 
 
 
