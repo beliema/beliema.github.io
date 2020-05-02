9d2f226ef7ee551e2b061c6ff52b4a5389c3ce97
Alina Zeiher <alina.lisa.zeiherweb.de>
2020-05-02 16:00:41 +0200

    ss

--- Semester2/A2/Memorie.js
+++ Semester2/A2/Memorie.js
@@ -6,26 +6,7 @@ function flipCard() {
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
 
-function flipCard() {
-if (lockBoard) return;
-if (this === firstCard) return;
-
-this.classList.add('flip');
-
-if (!hasFlippedCard) {
-  hasFlippedCard = true;
-  firstCard = this;
-
-  return;
-}
-
-secondCard = this;
-checkForMatch();
-}
 
 
 
