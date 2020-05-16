a7e402bbe61526b0b2156c5a78d4cc3637970c18
Alina Zeiher <alina.lisa.zeiherweb.de>
2020-05-16 12:29:05 +0200

    ss

--- Semester2/A4/haushilfe.js
+++ Semester2/A4/haushilfe.js
@@ -1,35 +1,73 @@
-function tableCreate() {
-    //body reference 
-    var body = document.getElementsByTagName("body")[0];
-  
-    // create elements <table> and a <tbody>
-    var tbl = document.createElement("table");
-    var tblBody = document.createElement("tbody");
-  
-    // cells creation
-    for (var j = 0; j <= 2; j++) {
-      // table row creation
-      var row = document.createElement("tr");
-  
-      for (var i = 0; i < 2; i++) {
-        // create element <td> and text node 
-        //Make text node the contents of <td> element
-        // put <td> at end of the table row
-        var cell = document.createElement("td");
-        var cellText = document.createTextNode("cell is row " + j + ", column " + i);
-  
-        cell.appendChild(cellText);
-        row.appendChild(cell);
-      }
-  
-      //row added to end of table body
-      tblBody.appendChild(row);
-    }
-  
-    // append the <tbody> inside the <table>
-    tbl.appendChild(tblBody);
-    // put <table> in the <body>
-    body.appendChild(tbl);
-    // tbl border attribute to 
-    tbl.setAttribute("border", "2");
+//Alles zum Warenkorb hinzufügen
+function addToCart(){
+
+var e = document.getElementById("articledropdown");
+var artikel = e.options[e.selectedIndex].text;
+
+var i = document.getElementById("wheretoget");
+var where = i.options[e.selectedIndex].text;
+
+var amount = document.getElementById("howmuch").value;
+
+
+  document.getElementById("amountend").innerHTML = amount;
+  document.getElementById("productend").innerHTML = artikel;
+  document.getElementById("whereend").innerHTML = where;
 }
+
+// Knopf Funktionen
+function showGrocery() {
+  var checkBox = document.getElementById("gamemode1");
+  var text = document.getElementById("groceryinput");
+
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+
+function showChores() {
+  var checkBox = document.getElementById("gamemode2");
+  var text = document.getElementById("choreInput");
+
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+//Dropdown Produkt
+$(function() {
+  var data = [
+    {
+      "product": "",
+      "id": "0",
+      "price": "",
+      "place": "",
+    },
+    {
+      "product": "Toilettenpapier",
+      "id": "1",
+      "price": "20",
+      "place": "Edeka",
+      },
+      {
+      "product": "Desinfektionsmittel",
+      "id": "2",
+      "price": "200",
+      "place": "Lidl",
+      }
+  ];
+  $.each(data, function(i, option) {
+      $('#articledropdown').append($('<option/>').attr("value", option.id).text(option.product));
+  });
+  $.each(data, function(i, option) {
+    $('#wheretoget').append($('<option/>').attr("value", option.id).text(option.place));
+  });
+})
+
+
+
