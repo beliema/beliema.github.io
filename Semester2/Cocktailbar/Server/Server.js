0cb8956e39fefa5d775010398a6a23290f4f448a
Alina Zeiher <alina.lisa.zeiherweb.de>
2020-05-25 15:45:08 +0200

    gg

--- Semester2/Cocktailbar/Server/Server.js
+++ Semester2/Cocktailbar/Server/Server.js
@@ -1,10 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Http = require("http");
-// import * as Url from "url";
+const Url = require("url");
 var L06_CocktailBar;
 (function (L06_CocktailBar) {
     let server = Http.createServer();
     console.log(server);
+    let port = process.env.PORT;
+    if (port == undefined)
+        port = 5001;
+    console.log("Server starting on port" + port);
+    server.listen(port);
+    server.addListener("request", handleRequest);
+    function handleRequest(_request, _response) {
+        console.log("Whats up?");
+        if (_request.url) {
+            let url = Url.parse(_request.url, true);
+            console.log(url.query);
+        }
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Acces-Control-Allow-Origin", "*");
+        _response.write("This is my response");
+        _response.end();
+    }
 })(L06_CocktailBar = exports.L06_CocktailBar || (exports.L06_CocktailBar = {}));
 //# sourceMappingURL=Server.js.map
