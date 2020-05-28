19d2063c4d13ae0e42fa0628638046f86807e13d
Alina Zeiher <alina.lisa.zeiherweb.de>
2020-05-28 12:02:28 +0200

    s

---
+++ Semester2/A6ts/Server/Server.js
@@ -0,0 +1,28 @@
+//# sourceMappingURL=Server.js.map
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Http = require("http");
+const Url = require("url");
+var L06_CocktailBar;
+(function (L06_Haushilfe) {
+    let server = Http.createServer();
+    console.log(server);
+    let port = process.env.PORT;
+    if (port == undefined)
+        port = 5001;
+    console.log("Server starting on port" + port);
+    server.listen(port);
+    server.addListener("request", handleRequest);
+    function handleRequest(_request, _response) {
+        console.log("Whats up?");
+        if (_request.url) {
+            let url = Url.parse(_request.url, true);
+            console.log(url.query);
+        }
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Acces-Control-Allow-Origin", "*");
+        _response.write("This is my response");
+        _response.end();
+    }
+})(L06_Haushilfe = exports.L06_Haushilfe || (exports.L06_Haushilfe = {}));
+//# sourceMappingURL=Server.js.map
