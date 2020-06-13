bfa8be4f4e571984cb9e3648c58c78434de60766
Alina Zeiher <alina.lisa.zeiherweb.de>
2020-06-13 16:08:26 +0200

    m

--- Semester2/A8/Virus/Virus.js
+++ Semester2/A8/Virus/Virus.js
@@ -1,59 +1,211 @@
-//Abgabe L08_Canvas; Alina Zeiher mit Hilfe von Code von Alida Kohler
-var L08_Virus;
-(function (L08_Virus) {
-    let canvas = document.querySelector("canvas");
-    let crc2 = canvas.getContext("2d");
+var L08_Canvas_Corona;
+(function (L08_Canvas_Corona) {
+    let crc2;
+    let middle = 0.95;
     let width;
     let height;
-    let middleheigth = 600; //Mitte der Vertikalen
-    let middlewigth = 500; //Mitte der Horizontalen
-    window.addEventListener("load", createImage);
-    function createImage() {
-        drawBackground();
-        createCells();
-    }
+    window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
-        let virus = document.querySelector("virus");
-        if (!virus)
+        let canvas = document.querySelector("canvas");
+        if (!canvas)
             return;
-        crc2 = virus.getContext("2d");
+        crc2 = canvas.getContext("2d");
+        drawCellBackground();
+        drawHumanCells({ x: 200, y: 300 }, { x: 100, y: 120 });
+        drawCoronaVirus({ x: 100, y: 150 }, { x: 70, y: 70 });
+        drawAntibodys({ x: 60, y: 420 }, { x: 100, y: 120 });
+        drawKillerCells({ x: 30, y: 370 }, { x: 100, y: 250 });
+        drawParticles({ x: 130, y: 490 }, { x: 260, y: 490 });
     }
-    function drawBackground() {
-        console.log("Hintergrund Lungengewebe");
-        let pattern = document.createElement('canvas').getContext('2d');
-        pattern.canvas.width = 100;
-        pattern.canvas.height = 40;
-        pattern.fillStyle = "#97a0db3a";
-        pattern.fillRect(0, 0, pattern.canvas.width, pattern.canvas.height);
-        pattern.moveTo(0, 20);
-        pattern.lineTo(20, 20);
-        pattern.lineTo(40, 0);
-        pattern.lineTo(60, 0);
-        pattern.lineTo(100, 20);
-        pattern.lineTo(60, 40);
-        pattern.lineTo(40, 40);
-        pattern.lineTo(20, 20);
+    // Funktion erstellen des Hintergrundes mit Farbverlauf und Pattern-Struktur 
+    function drawCellBackground() {
+        console.log("background");
+        //Farbverlauf von hellrot oben bis weiß unten: 
         let gradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
-        gradient.addColorStop(0, "lightred");
-        gradient.addColorStop(middleheigth, "lightpink");
-        gradient.addColorStop(1, "red");
+        gradient.addColorStop(0, "#FA5858");
+        gradient.addColorStop(middle, "#F8E0E6");
         crc2.fillStyle = gradient;
         crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
-        //Pattern for Cell Membran
-        pattern.strokeStyle = "#88888844";
+        // Einbettung eines Patterns 
+        let pattern = document.createElement("canvas").getContext("2d");
+        pattern.canvas.width = 40;
+        pattern.canvas.height = 20;
+        pattern.fillStyle = "#97a0db1a";
+        pattern.fillRect(0, 0, pattern.canvas.width, pattern.canvas.height);
+        pattern.moveTo(0, 10);
+        pattern.lineTo(10, 0);
+        pattern.lineTo(20, 10);
+        pattern.lineTo(30, 0);
+        pattern.lineTo(40, 20);
+        pattern.lineTo(30, 20);
+        pattern.lineTo(20, 10);
+        pattern.lineTo(10, 10);
+        pattern.stroke();
+        pattern.strokeStyle = "#E6E6E6";
         pattern.stroke();
         pattern.closePath();
+        crc2.fillStyle = crc2.createPattern(pattern.canvas, "repeat");
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
     }
-    function resizeCanvas() {
-        width = window.innerWidth;
-        canvas.setAttribute("width", width + "px");
-        height = window.innerHeight;
-        canvas.setAttribute("height", height + "px");
-        //Background-Color: 
-        crc2.fillStyle = "#DEEFF5";
-        crc2.fillRect(0, 0, width, height);
+    //Funktion 2: Zeichnen der menschlichen Zellen; Orientierung an Sonne in L08_CanvasAlley
+    function drawHumanCells(_position, _size) {
+        console.log("menschliche Zellen werden gezeichnet", _position);
+        let r1 = 1; //Radius innen 
+        let r2 = 13; // Radius außen
+        let nParticles = 15; //Anzahl menschlicher Zellen
+        let particle = new Path2D();
+        let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2); //erstelle einen radialen Verlauf 
+        particle.arc(0, 0, r2, 0, 2 * Math.PI); //Zelle als voller Kreis -> 2x Pi 
+        // Farbverlauf, Leichte Transparenz nach außen
+        gradient.addColorStop(0, "#B895A3");
+        gradient.addColorStop(0.5, "#FFFFFF");
+        gradient.addColorStop(1, "#2E2E2E");
+        crc2.save(); //Zwischenspeichern 
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        for (let drawn = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x = (Math.random() - 0.5) * _size.x;
+            let y = -(Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+        crc2.restore();
     }
-    function createCells() {
+    //Funktion: Zeichnen des Corona-Virus
+    function drawCoronaVirus(_position, _size) {
+        console.log("Coronaviruszellen werden gezeichnet", _position);
+        let r1 = 1; //Radius innen 
+        let r2 = 25; // Radius außen
+        let nParticles = 6; //Anzahl Coronaviruszellen
+        let particle = new Path2D();
+        let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2); //erstelle einen radialen Verlauf 
+        particle.arc(0, 0, r2, 0, 2 * Math.PI); //Zelle als voller Kreis -> 2x Pi 
+        gradient.addColorStop(0, "#B895A3");
+        gradient.addColorStop(0.6, "#596024");
+        gradient.addColorStop(1, "#42452C");
+        crc2.save(); //Zwischenspeichern 
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        for (let drawn = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x = (Math.random() - 0.5) * _size.x;
+            let y = -(Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+        crc2.restore();
     }
-})(L08_Virus || (L08_Virus = {}));
+    //Zeichnen der Antikörper
+    function drawAntibodys(_position, _size) {
+        console.log("Antikörperzellen werden gezeichnet", _position);
+        let r1 = 1; //Radius innen 
+        let r2 = 20; // Radius außen
+        let nParticles = 8; //Anzahl Antikörper
+        let particle = new Path2D();
+        let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2); //erstelle einen radialen Verlauf 
+        particle.arc(0, 0, r2, 0, 2 * Math.PI); //Zelle als voller Kreis -> 2x Pi 
+        gradient.addColorStop(0, "#FFFFFF");
+        gradient.addColorStop(0.6, "#FFFFFF");
+        gradient.addColorStop(1, "#F78181");
+        crc2.save(); //Zwischenspeichern 
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        for (let drawn = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x = (Math.random() - 0.5) * _size.x;
+            let y = -(Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+        crc2.restore();
+    }
+    // function drawAntibodys(_position: Vector, _size: Vector):void {
+    //     console.log("Antikörperzellen", _position, _size);
+    //    let amountA: number = 10;
+    //    let antibody: Path2D = new Path2D;
+    //    crc2.beginPath();
+    //    crc2.moveTo(_position.x, _position.y);
+    //    crc2.lineTo(_position.x + 29, _position.y - 4 );
+    // crc2.lineTo(_position.x + 40, _position.y +20);
+    //    crc2.lineWidth = 3;
+    //    crc2.strokeStyle = "#596024";
+    //    crc2.stroke();
+    //   crc2.closePath();
+    //   crc2.beginPath();
+    //   crc2.arc(_position.x + 14, _position.y - 6, 17, 0, 2 * Math.PI);
+    //   crc2.stroke();
+    //   crc2.closePath();
+    //  crc2.beginPath();
+    //   crc2.lineWidth = 3; //Breite des Striches
+    //   crc2.lineCap = "round"; //Art des Striches 
+    //   crc2.moveTo(_position.x, _position.y); // 60 = Länge des Striches; 55 = Steigung des Striches
+    //   crc2.lineTo(_position.x +27,_position.y -15);
+    //   crc2.stroke();
+    //   crc2.save();
+    //   crc2.translate(_position.x, _position.y);
+    //  for (let drawn: number = 0; drawn < amountA; drawn++) {
+    //      crc2.save();
+    //      let x: number = (Math.random() - 0.5) * _size.x;
+    //      let y: number = - (Math.random() * _size.y);
+    //      crc2.translate(x, y);
+    //      crc2.fill(antibody);
+    //      crc2.restore();
+    // }
+    // crc2.restore();
+    // }
+    //Funktion Erstellen der KillerZellen
+    function drawKillerCells(_position, _size) {
+        console.log("Coronaviruszellen werden gezeichnet", _position);
+        let r1 = 1; //Radius innen 
+        let r2 = 15; // Radius außen
+        let nParticles = 7; //Anzahl Killerzellen
+        let particle = new Path2D();
+        let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2); //erstelle einen radialen Verlauf 
+        particle.arc(0, 0, r2, 0, 2 * Math.PI); //Zelle als voller Kreis -> 2x Pi 
+        gradient.addColorStop(0, "#000000"); //Innen
+        gradient.addColorStop(0.4, "#FFFFFF"); // circa Mitte
+        gradient.addColorStop(0.8, "#F5A9BC"); //außen
+        crc2.save(); //Zwischenspeichern 
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        for (let drawn = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x = (Math.random() - 0.5) * _size.x;
+            let y = -(Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+        crc2.restore();
+    }
+    //Zuletzt: Parikel auf Oberster Ebene 
+    function drawParticles(_position, _size) {
+        console.log("particles", _position, _size);
+        let r1 = 1;
+        let r2 = 8;
+        let nParticles = 40;
+        let particle = new Path2D();
+        let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
+        particle.arc(0, 0, r2, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "HSL(0, 50%, 100%)");
+        gradient.addColorStop(0.8, "HSLA(360, 40%, 60%)");
+        gradient.addColorStop(0.9, "HSLA(360, 40%, 60%)");
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        for (let drawn = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x = (Math.random() - 0.5) * _size.x;
+            let y = -(Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+        crc2.restore();
+    }
+})(L08_Canvas_Corona || (L08_Canvas_Corona = {}));
 //# sourceMappingURL=Virus.js.map
--- Semester2/A8/Virus/Virus.ts
+++ Semester2/A8/Virus/Virus.ts
@@ -1,85 +1,316 @@
-//Abgabe L08_Canvas; Alina Zeiher mit Hilfe von Code von Alida Kohler
-
-namespace L08_Virus {
-
-    let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.querySelector("canvas");
-    let crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D>canvas.getContext("2d");
+namespace L08_Canvas_Corona {
+    interface Vector {
+        x: number;
+        y: number;
+    }
+    let crc2: CanvasRenderingContext2D;
+    let middle: number = 0.95;
     let width: number;
     let height: number;
-    let middleheigth: number = 600;  //Mitte der Vertikalen
-    let middlewigth: number= 500;   //Mitte der Horizontalen
+    
+    
 
-
-    window.addEventListener("load", createImage);
-
-
-    function createImage(): void {
+    window.addEventListener("load", handleLoad);
+    
+    
+    function handleLoad (_event: Event): void {
+        let canvas: HTMLCanvasElement = <HTMLCanvasElement> document.querySelector("canvas");
+        if (!canvas)
+            return;
+        crc2 = <CanvasRenderingContext2D>canvas.getContext("2d");
         
-        drawBackground();
-        createCells();
+        
+
+        drawCellBackground();
+        drawHumanCells({x: 200, y: 300}, {x: 100, y: 120});
+        drawCoronaVirus({x: 100, y: 150}, {x: 70, y: 70});
+        drawAntibodys({x: 60, y: 420}, {x: 100, y: 120});
+        drawKillerCells({x: 30, y: 370}, {x: 100, y: 250});
+        drawParticles({x: 130, y: 490}, {x: 260, y: 490});
+
+
+
     }
-   
+    
+    // Funktion erstellen des Hintergrundes mit Farbverlauf und Pattern-Struktur 
+
+    function drawCellBackground(): void {
+        console.log("background");
+
+       //Farbverlauf von hellrot oben bis weiß unten: 
 
     
-    function handleLoad(_event: Event): void {
-        let virus: HTMLCanvasElement | null = document.querySelector("virus");
-        if (!virus)
-            return;
-        crc2 = <CanvasRenderingContext2D>virus.getContext("2d");
-
-        
-
-    }
+        let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height); 
+        gradient.addColorStop(0, "#FA5858");
+        gradient.addColorStop(middle, "#F8E0E6");
        
 
 
-        function drawBackground(): void {
-            console.log("Hintergrund Lungengewebe");
-
-            let pattern: CanvasRenderingContext2D = <CanvasRenderingContext2D>document.createElement('canvas').getContext('2d');
-            pattern.canvas.width = 100;
-            pattern.canvas.height = 40;
-            pattern.fillStyle = "#97a0db3a";
-            pattern.fillRect(0, 0, pattern.canvas.width, pattern.canvas.height);
-            pattern.moveTo(0, 20);
-            pattern.lineTo(20, 20);
-            pattern.lineTo(40, 0);
-            pattern.lineTo(60, 0);
-            pattern.lineTo(100, 20);
-            pattern.lineTo(60, 40);
-            pattern.lineTo(40, 40);
-            pattern.lineTo(20, 20);
-
-            let gradient: CanvasGradient = crc2.createLinearGradient( 0,0,0, crc2.canvas.height);
-            gradient.addColorStop(0, "lightred");
-            gradient.addColorStop(middleheigth, "lightpink");
-            gradient.addColorStop(1, "red");
-
-            crc2.fillStyle = gradient;
-            crc2.fillRect(0,0, crc2.canvas.width, crc2.canvas.height);
+        crc2.fillStyle = gradient;
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
         
-            //Pattern for Cell Membran
-            pattern.strokeStyle = "#88888844";
-            pattern.stroke();
-            pattern.closePath();
-        }
+        // Einbettung eines Patterns 
+
+        let pattern: CanvasRenderingContext2D = <CanvasRenderingContext2D>document.createElement("canvas").getContext("2d");
+        
+        pattern.canvas.width = 40;
+        pattern.canvas.height = 20;
+
+        pattern.fillStyle = "#97a0db1a";
+        pattern.fillRect(0, 0, pattern.canvas.width, pattern.canvas.height);
+        pattern.moveTo(0, 10);
+        pattern.lineTo(10, 0);
+        pattern.lineTo(20, 10);
+        pattern.lineTo(30, 0);
+        pattern.lineTo(40, 20);
+        pattern.lineTo(30, 20);
+        pattern.lineTo(20, 10);
+        pattern.lineTo(10, 10);
+        pattern.stroke();
+
+        pattern.strokeStyle = "#E6E6E6";
+        pattern.stroke();
+        pattern.closePath();
+
+        crc2.fillStyle = <CanvasRenderingContext2D>crc2.createPattern(pattern.canvas, "repeat");
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+
+    }
+
+    //Funktion 2: Zeichnen der menschlichen Zellen; Orientierung an Sonne in L08_CanvasAlley
+
+
+    function drawHumanCells(_position: Vector, _size: Vector): void {
+        console.log("menschliche Zellen werden gezeichnet", _position);
+        let r1: number = 1; //Radius innen 
+        let r2: number = 13; // Radius außen
+        let nParticles: number = 15; //Anzahl menschlicher Zellen
+        let particle: Path2D = new Path2D();
+        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2); //erstelle einen radialen Verlauf 
+        particle.arc(0, 0, r2, 0, 2 * Math.PI); //Zelle als voller Kreis -> 2x Pi 
+
+        // Farbverlauf, Leichte Transparenz nach außen
+        gradient.addColorStop(0, "#B895A3");
+        gradient.addColorStop(0.5, "#FFFFFF"); 
+        gradient.addColorStop(1, "#2E2E2E");
+        
+        
+ 
+
+
+        crc2.save(); //Zwischenspeichern 
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        
+        
         
 
-        function resizeCanvas(): void {
-            width= window.innerWidth;
-            canvas.setAttribute("width", width + "px");
-            height = window.innerHeight;
-            canvas.setAttribute("height", height + "px");
-            //Background-Color: 
-            crc2.fillStyle ="#DEEFF5";
-            crc2.fillRect(0,0,width,height);
+        for (let drawn: number = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x: number = (Math.random() - 0.5) * _size.x;
+            let y: number = - (Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
         }
+        crc2.restore();
 
+    }
 
+    //Funktion: Zeichnen des Corona-Virus
+
+    function drawCoronaVirus(_position: Vector, _size: Vector): void {
+        console.log("Coronaviruszellen werden gezeichnet", _position);
+        let r1: number = 1; //Radius innen 
+        let r2: number = 25; // Radius außen
+        let nParticles: number = 6; //Anzahl Coronaviruszellen
+        let particle: Path2D = new Path2D();
+        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2); //erstelle einen radialen Verlauf 
+        particle.arc(0, 0, r2, 0, 2 * Math.PI); //Zelle als voller Kreis -> 2x Pi 
+        gradient.addColorStop(0, "#B895A3");
+        gradient.addColorStop(0.6, "#596024"); 
+        gradient.addColorStop(1, "#42452C");
         
-        function createCells(): void {
+        
+ 
 
 
+        crc2.save(); //Zwischenspeichern 
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        
+        
+        
+
+        for (let drawn: number = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x: number = (Math.random() - 0.5) * _size.x;
+            let y: number = - (Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
         }
+        crc2.restore();
+
+    }
+
+    //Zeichnen der Antikörper
+
+    function drawAntibodys(_position: Vector, _size: Vector): void {
+        console.log("Antikörperzellen werden gezeichnet", _position);
+        let r1: number = 1; //Radius innen 
+        let r2: number = 20; // Radius außen
+        let nParticles: number = 8; //Anzahl Antikörper
+        let particle: Path2D = new Path2D();
+        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2); //erstelle einen radialen Verlauf 
+        particle.arc(0, 0, r2, 0, 2 * Math.PI); //Zelle als voller Kreis -> 2x Pi 
+        gradient.addColorStop(0, "#FFFFFF");
+        gradient.addColorStop(0.6, "#FFFFFF"); 
+        gradient.addColorStop(1, "#F78181");
+        
+        
+ 
+
+
+        crc2.save(); //Zwischenspeichern 
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        
+        
+        
+
+        for (let drawn: number = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x: number = (Math.random() - 0.5) * _size.x;
+            let y: number = - (Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+        crc2.restore();
+
+    }
+
+
+
+   // function drawAntibodys(_position: Vector, _size: Vector):void {
+   //     console.log("Antikörperzellen", _position, _size);
+
+    //    let amountA: number = 10;
+    //    let antibody: Path2D = new Path2D;
+    //    crc2.beginPath();
+    //    crc2.moveTo(_position.x, _position.y);
+    //    crc2.lineTo(_position.x + 29, _position.y - 4 );
+        // crc2.lineTo(_position.x + 40, _position.y +20);
+    //    crc2.lineWidth = 3;
+    //    crc2.strokeStyle = "#596024";
+    //    crc2.stroke();
+    //   crc2.closePath();
+        
+
+     //   crc2.beginPath();
+     //   crc2.arc(_position.x + 14, _position.y - 6, 17, 0, 2 * Math.PI);
+     //   crc2.stroke();
+     //   crc2.closePath();
+
+      //  crc2.beginPath();
+     //   crc2.lineWidth = 3; //Breite des Striches
+     //   crc2.lineCap = "round"; //Art des Striches 
+     //   crc2.moveTo(_position.x, _position.y); // 60 = Länge des Striches; 55 = Steigung des Striches
+     //   crc2.lineTo(_position.x +27,_position.y -15);
+     //   crc2.stroke();
+
+
+
+     //   crc2.save();
+     //   crc2.translate(_position.x, _position.y);
+
+     
+      //  for (let drawn: number = 0; drawn < amountA; drawn++) {
+      //      crc2.save();
+      //      let x: number = (Math.random() - 0.5) * _size.x;
+      //      let y: number = - (Math.random() * _size.y);
+      //      crc2.translate(x, y);
+      //      crc2.fill(antibody);
+      //      crc2.restore();
+           
+       // }
+       // crc2.restore();
+   // }
+
+    //Funktion Erstellen der KillerZellen
+
+    function drawKillerCells(_position: Vector, _size: Vector): void {
+        console.log("Coronaviruszellen werden gezeichnet", _position);
+        let r1: number = 1; //Radius innen 
+        let r2: number = 15; // Radius außen
+        let nParticles: number = 7; //Anzahl Killerzellen
+        let particle: Path2D = new Path2D();
+        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2); //erstelle einen radialen Verlauf 
+        particle.arc(0, 0, r2, 0, 2 * Math.PI); //Zelle als voller Kreis -> 2x Pi 
+        gradient.addColorStop(0, "#000000"); //Innen
+        gradient.addColorStop(0.4, "#FFFFFF"); // circa Mitte
+        gradient.addColorStop(0.8, "#F5A9BC"); //außen
+        
+        
+ 
+
+
+        crc2.save(); //Zwischenspeichern 
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        
+        
+        
+
+        for (let drawn: number = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x: number = (Math.random() - 0.5) * _size.x;
+            let y: number = - (Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+        crc2.restore();
+
+    }
+
+    //Zuletzt: Parikel auf Oberster Ebene 
     
+    function drawParticles(_position: Vector, _size: Vector): void {
+        console.log("particles", _position, _size);
+        let r1: number = 1;
+        let r2: number = 8;
+        let nParticles: number = 40;
+        let particle: Path2D = new Path2D();
+        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
+        particle.arc(0, 0, r2, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "HSL(0, 50%, 100%)");
+        gradient.addColorStop(0.8, "HSLA(360, 40%, 60%)"); 
+        gradient.addColorStop(0.9, "HSLA(360, 40%, 60%)");
+        
+        
+ 
+
+
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        
+        
+        
+
+        for (let drawn: number = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x: number = (Math.random() - 0.5) * _size.x;
+            let y: number = - (Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+        crc2.restore();
+
+    }
+
+
 }
